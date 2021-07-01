5123029be1fc76eead80f77719d7a87af4a5356c
Tomislav Mijatov <mijatovtomislav@gmail.com>
2021-07-01 12:04:20 +0200

    race details

--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -5,23 +5,34 @@ export default class RaceDetails extends React.Component {
     constructor() {
         super();
         this.state = {
-            race:[],
-            urlRaceQualification:[]
+            raceResults:[],
+            raceQualification:[]
         }
     };
     componentDidMount(){
         this.getRaceDetails(this.props.match.params.id);
+        
     };
     getRaceDetails(id) {
         
-        var urlRaceQualification = $.ajax(`http://ergast.com/api/f1/2013/${id}/results.json`);
-        
-        $.when(urlRaceQualification).done()
+        var urlRaceQualification = $.ajax(`http://ergast.com/api/f1/2013/${id}/qualifying.json`);
+        var urlRaceResults = $.ajax(`http://ergast.com/api/f1/2013/${id}/results.json`);
+
+        $.when(urlRaceQualification,urlRaceResults).done(
+            function(dataRaceQualification,dataRaceResults){
+                console.log("link",dataRaceResults);
+                console.log("kvalif",dataRaceQualification);
+                this.setState({
+                     raceQualification: dataRaceQualification[0].MRData.RaceTable.Races[0],
+                     raceResults: dataRaceResults[0].MRData.Races.Races[0]
+                });
+            }
+        )   
     }
     render () {
         return (
             <div>
-
+                
             </div>
         );
 
