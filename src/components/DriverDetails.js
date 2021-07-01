f7f33647c5520bf4915ba7bf6dc6cc0ab51fb703
Tomislav Mijatov <mijatovtomislav@gmail.com>
2021-07-01 12:50:39 +0200

    added race detail

--- src/components/DriverDetails.js
+++ src/components/DriverDetails.js
@@ -3,137 +3,136 @@ import * as $ from "jquery";
 import Loader from "react-loader-spinner";
 
 export default class DriverDetails extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      driver: [],
-      constructors: [],
-      races: [],
-      isLoaded: false,
-    };
-  }
-
-  componentDidMount() {
-    this.getDriverDetails(this.props.match.params.id);
-  }
-
-  getDriverDetails(id) {
-    var urlDriversDetails = $.ajax(
-      `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`
-    );
-    var urlDriversRaces = $.ajax(
-      `http://ergast.com/api/f1/2013/drivers/${id}/results.json`
-    );
-
-    $.when(urlDriversDetails, urlDriversRaces).done(
-      function (dataDriversDetails, dataDriversRaces) {
-        this.setState({
-          driver:
-            dataDriversDetails[0].MRData.StandingsTable.StandingsLists[0]
-              .DriverStandings[0].Driver,
-          constructor:
-            dataDriversDetails[0].MRData.StandingsTable.StandingsLists[0]
-              .DriverStandings[0].Constructors[0],
-          races: dataDriversRaces[0].MRData.RaceTable.Races,
-          isLoaded: true,
-        });
-      }.bind(this)
-    );
-  }
-  //     $.get(url, (data) => {
-  //       this.setState({
-  //         driver:
-  //           data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
-  //             .Driver,
-  //         constructor:
-  //           data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
-  //             .Constructors[0],
-  //       });
-  //     });
-  //   }
-
-  //   getRaces() {
-  //     var url = `http://ergast.com/api/f1/2013/drivers/${this.props.match.params.id}/results.json`;
-  //     $.get(url, (data) => {
-  //       this.setState({
-  //         races: data.MRData.RaceTable.Races,
-  //       });
-  //     });
-  //   }
-
-  render() {
-    if (!this.state.isLoaded) {
-      return <Loader type="Puff" color="#00BFFF" height={100} width={100} />;
+    constructor() {
+        super();
+        this.state = {
+            driver: [],
+            constructors: [],
+            races: [],
+            isLoaded: false,
+        };
     }
-    console.log(this.state.driver.familyName);
-    console.log(this.state.driver.givenName);
-    return (
-      <div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <td colSpan="2">
-                  <img
-                    alt="driver picture"
-                    src={`../assets/img/${
-                      this.state.driver.givenName
-                    }_${this.state.driver.familyName.replaceAll(` `, `_`)}.jpg`}
-                  ></img>
-                  {this.state.driver.givenName} {this.state.driver.familyName}
-                </td>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Country:</td>
-                <td>{this.state.driver.nationality}</td>
-              </tr>
-              <tr>
-                <td>Team:</td>
-                <td>{this.state.constructor.name}</td>
-              </tr>
-              <tr>
-                <td>Birth:</td>
-                <td>{this.state.driver.dateOfBirth}</td>
-              </tr>
-              <tr>
-                <td>Biography:</td>
-                <td>link</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-        <div>
-          <table>
-            <thead>
-              <tr>
-                <td>Formula 1 2013 Results</td>
-              </tr>
-              <tr>
-                <th>Round</th>
-                <th>GrandPrix</th>
-                <th>Team</th>
-                <th>Grid</th>
-                <th>Race</th>
-              </tr>
-            </thead>
-            <tbody>
-              {this.state.races.map((race, i) => {
-                return (
-                  <tr key={i}>
-                    <td>{race.round}</td>
-                    <td>{race.raceName}</td>
-                    <td>{race.Results[0].Constructor.name}</td>
-                    <td>{race.Results[0].grid}</td>
-                    <td>{race.Results[0].position}</td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </div>
-    );
-  }
+
+    componentDidMount() {
+        this.getDriverDetails(this.props.match.params.id);
+    }
+
+    getDriverDetails(id) {
+        var urlDriversDetails = $.ajax(
+            `http://ergast.com/api/f1/2013/drivers/${id}/driverStandings.json`
+        );
+        var urlDriversRaces = $.ajax(
+            `http://ergast.com/api/f1/2013/drivers/${id}/results.json`
+        );
+
+        $.when(urlDriversDetails, urlDriversRaces).done(
+            function (dataDriversDetails, dataDriversRaces) {
+                this.setState({
+                    driver:
+                        dataDriversDetails[0].MRData.StandingsTable.StandingsLists[0]
+                            .DriverStandings[0].Driver,
+                    constructor:
+                        dataDriversDetails[0].MRData.StandingsTable.StandingsLists[0]
+                            .DriverStandings[0].Constructors[0],
+                    races: dataDriversRaces[0].MRData.RaceTable.Races,
+                    isLoaded: true,
+                });
+            }.bind(this)
+        );
+    }
+    //     $.get(url, (data) => {
+    //       this.setState({
+    //         driver:
+    //           data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
+    //             .Driver,
+    //         constructor:
+    //           data.MRData.StandingsTable.StandingsLists[0].DriverStandings[0]
+    //             .Constructors[0],
+    //       });
+    //     });
+    //   }
+
+    //   getRaces() {
+    //     var url = `http://ergast.com/api/f1/2013/drivers/${this.props.match.params.id}/results.json`;
+    //     $.get(url, (data) => {
+    //       this.setState({
+    //         races: data.MRData.RaceTable.Races,
+    //       });
+    //     });
+    //   }
+
+    render() {
+        if (!this.state.isLoaded) {
+            return <Loader type="Puff" color="#00BFFF" height={100} width={100} />;
+        }
+        console.log(this.state.driver.familyName);
+        console.log(this.state.driver.givenName);
+        return (
+            <div>
+                <div>
+                    <img
+                        alt="driver picture"
+                        src={`../assets/img/${this.state.driver.givenName
+                            }_${this.state.driver.familyName.replaceAll(` `, `_`)}.jpg`}
+                    ></img>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td colSpan="2">
+                                    {this.state.driver.givenName} {this.state.driver.familyName}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Country:</td>
+                                <td>{this.state.driver.nationality}</td>
+                            </tr>
+                            <tr>
+                                <td>Team:</td>
+                                <td>{this.state.constructor.name}</td>
+                            </tr>
+                            <tr>
+                                <td>Birth:</td>
+                                <td>{this.state.driver.dateOfBirth}</td>
+                            </tr>
+                            <tr>
+                                <td>Biography:</td>
+                                <td>link</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>Formula 1 2013 Results</td>
+                            </tr>
+                            <tr>
+                                <th>Round</th>
+                                <th>GrandPrix</th>
+                                <th>Team</th>
+                                <th>Grid</th>
+                                <th>Race</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {this.state.races.map((race, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{race.round}</td>
+                                        <td>{race.raceName}</td>
+                                        <td>{race.Results[0].Constructor.name}</td>
+                                        <td>{race.Results[0].grid}</td>
+                                        <td>{race.Results[0].position}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        );
+    }
 }
--- src/components/RaceDetails.js
+++ src/components/RaceDetails.js
@@ -6,37 +6,78 @@ export default class RaceDetails extends React.Component {
     constructor() {
         super();
         this.state = {
-            raceResults:[],
-            raceQualification:[],
+            raceResults: [],
+            raceQualification: [],
             isLoaded: false
         }
     };
-    componentDidMount(){
+    componentDidMount() {
         this.getRaceDetails(this.props.match.params.id);
-        
+
     };
     getRaceDetails(id) {
-        if (!this.state.isLoaded) {
-            return <Loader type="Puff" color="#00BFFF" height={100} width={100} />;
-          }        
+
         var urlRaceQualification = $.ajax(`http://ergast.com/api/f1/2013/${id}/qualifying.json`);
         var urlRaceResults = $.ajax(`http://ergast.com/api/f1/2013/${id}/results.json`);
 
-        $.when(urlRaceQualification,urlRaceResults).done(
-            function(dataRaceQualification,dataRaceResults){
-                console.log("link",dataRaceResults);
-                console.log("kvalif",dataRaceQualification);
+        $.when(urlRaceQualification, urlRaceResults).done(
+            function (dataRaceQualification, dataRaceResults) {
+                console.log("link", dataRaceResults);
+                console.log("kvalif", dataRaceQualification);
                 this.setState({
-                     raceQualification: dataRaceQualification[0].MRData.RaceTable.Races[0],
-                     raceResults: dataRaceResults[0].MRData.Races.Races[0]
+                    raceQualification: dataRaceQualification[0].MRData.RaceTable.Races[0],
+                    raceResults: dataRaceResults[0].MRData.RaceTable.Races[0],
+                    isLoaded: true
                 });
-            }
-        )   
+            }.bind(this)
+        );
     }
-    render () {
+    render() {
+        if (!this.state.isLoaded) {
+            return <Loader type="Puff" color="#00BFFF" height={100} width={100} />;
+        }
         return (
             <div>
+                <div>
+                    <img
+                        alt="race picture"
+                        src={`...`}
+                    ></img>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td colSpan="2">
+                                    {this.state.raceQualification.raceName}
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Country:</td>
+                                <td>{this.state.raceQualification.Circuit.Location.country}</td>
+                            </tr>
+                            <tr>
+                                <td>Location:</td>
+                                <td>{this.state.raceQualification.Circuit.Location.locality}</td>
+                            </tr>
+                            <tr>
+                                <td>Date:</td>
+                                <td>{this.state.raceQualification.date}</td>
+                            </tr>
+                            <tr>
+                                <td>Full Report:</td>
+                                <td><a href={this.state.raceQualification.Circuit.url}>link</a></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
 
+                <div>
+                    
+                </div>
+                <div>
+
+                </div>
             </div>
         );
 
