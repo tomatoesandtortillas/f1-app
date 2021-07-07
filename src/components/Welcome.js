88cec3cb0f4ef9f3e496fbfdcb496247490e73ed
teasmiljanic <tea021@hotmail.com>
2021-07-07 08:52:40 +0200

    frontPage

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,8 +4,9 @@ import React from "react";
 export default class Welcome extends React.Component {
     render() {
         return (
-            <div className="welcome-page">
-                <h1>Welcome to F1</h1>
+            <div className="welcome-page" style={{
+                backgroundImage: "url(" + "../assets/img/frontPage.jpg" + ")",
+            }}>
             </div>
         );
     }
